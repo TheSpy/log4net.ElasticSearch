7f9cc1195b137343d2ee84d8bcedc3c41a193cd9
JP Toto <james.p.toto@gmail.com>
2013-11-01 22:36:47 -0400

    Added 3.0 support

---
+++ src/ConsoleApplication30/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using log4net;
+
+namespace ConsoleApplication30
+{
+    class Program
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            _log.Info("console");
+        }
+    }
+}
